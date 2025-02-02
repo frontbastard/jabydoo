4f881ddd952c14fc7df4ecad31353e0a77a0af09
pavlo <frontbastard@gmail.com>
2025-02-02 22:09:45 +0100

    Add a language switcher

--- site_service/settings.py
+++ site_service/settings.py
@@ -113,6 +113,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
 LANGUAGE_CODE = "en"
+# USE_I18N = True
+# USE_L10N = True
 LANGUAGES = [
     ("en", _("English")),
     ("fr", _("French")),
