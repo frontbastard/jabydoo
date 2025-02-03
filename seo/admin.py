f95432688a9c1539c7b8d161f408bf1755b7df7a
pavlo <frontbastard@gmail.com>
2025-02-03 10:25:09 +0100

    Make the seo inlines translatable

--- seo/admin.py
+++ seo/admin.py
@@ -1,8 +1,11 @@
 from django.contrib.contenttypes.admin import GenericStackedInline
+from parler.admin import TranslatableStackedInline
+
 from .models import SEO
 
 
-class SEOInline(GenericStackedInline):
+class SEOInline(GenericStackedInline, TranslatableStackedInline):
     model = SEO
     extra = 1
     max_num = 1
+    fields = ["title", "description", "keywords"]
--- seo/models.py
+++ seo/models.py
@@ -1,22 +1,21 @@
 from django.db import models
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
+from parler.models import TranslatableModel, TranslatedFields
 
 
-class SEOModel(models.Model):
-    title = models.CharField(max_length=200, blank=True)
-    description = models.TextField(blank=True)
-    keywords = models.CharField(max_length=500, blank=True)
+class SEO(TranslatableModel):
+
+    translations = TranslatedFields(
+        title=models.CharField(max_length=200, blank=True),
+        description=models.TextField(blank=True),
+        keywords=models.CharField(max_length=500, blank=True),
+    )
 
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey("content_type", "object_id")
 
-    class Meta:
-        abstract = True
-
-
-class SEO(SEOModel):
     class Meta:
         unique_together = ["content_type", "object_id"]
 
