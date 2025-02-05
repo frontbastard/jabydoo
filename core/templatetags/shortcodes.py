6f56759dd389c76a0b05ea26c303e14880ac7893
pavlo <frontbastard@gmail.com>
2025-02-05 23:11:06 +0100

    Create the shortcodes functionality
    - tested shortcode for logo in the header

--- core/templatetags/shortcodes.py
+++ core/templatetags/shortcodes.py
@@ -1,15 +1,17 @@
 from django import template
-from django.conf import settings
+
+from core.models import SiteOptions
 
 register = template.Library()
 
 
 @register.filter(name="sponsor_info")
 def sponsor_info(value):
-    if '[sponsor_url]' in value:
-        value = value.replace('[sponsor_url]', settings.SPONSOR_URL)
-    if '[sponsor_name]' in value:
-        value = value.replace('[sponsor_name]', settings.SPONSOR_NAME)
-    if '[sponsor_link]' in value:
-        value = value.replace('[sponsor_link]', f"<a href='{settings.SPONSOR_URL}'>{settings.SPONSOR_NAME}</a>")
+    options = SiteOptions.get_options()
+    if "[sponsor_url]" in value:
+        value = value.replace("[sponsor_url]", options.sponsor_url)
+    if "[sponsor_name]" in value:
+        value = value.replace("[sponsor_name]", options.sponsor_name)
+    if "[sponsor_link]" in value:
+        value = value.replace("[sponsor_link]", f"<a href='{options.sponsor_url}'>{options.sponsor_name}</a>")
     return value
