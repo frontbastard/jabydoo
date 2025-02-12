8b97aeae5e4052c1300b59b9efc32c01ed236c16
pavlo <frontbastard@gmail.com>
2025-02-12 13:33:00 +0100

    Fix the images path folder

--- site_service/settings.py
+++ site_service/settings.py
@@ -172,7 +172,7 @@ FILER_STORAGES = {
             "ENGINE": "filer.storage.PublicFileSystemStorage",
             "OPTIONS": {
                 "location": os.path.join(MEDIA_ROOT, "filer"),
-                "base_url": "/mediafiles/filer/",
+                "base_url": "/media/filer/",
             },
         },
     },
