5e09d0bbccf693cff64f4bf0562b8d50141edde2
pavlo <frontbastard@gmail.com>
2025-02-16 17:44:46 +0100

    Fix "page does not have a translation" error

--- core/management/commands/create_trust_pages.py
+++ core/management/commands/create_trust_pages.py
@@ -1,6 +1,5 @@
 from django.core.management.base import BaseCommand
 from django.utils.text import slugify
-from django.conf import settings
 from pages.models import Page
 
 TRUST_PAGES = [
@@ -12,8 +11,6 @@ TRUST_PAGES = [
     {"title": "Cookie Policy"},
 ]
 
-LANGUAGES = [lang[0] for lang in settings.LANGUAGES]
-
 
 class Command(BaseCommand):
     help = "Creates default trust pages if they do not exist."
@@ -21,11 +18,12 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         for page_data in TRUST_PAGES:
             slug = slugify(page_data["title"])
-
-            # Check if a page with this word exists
             page, created = Page.objects.get_or_create(slug=slug)
 
-            if created:
-                self.stdout.write(self.style.SUCCESS(f'Page "{page.title}" created.'))
-            else:
-                self.stdout.write(self.style.WARNING(f'Page "{page.title}" already exists.'))
+            if not page.has_translation("en"):
+                page.set_current_language("en")
+                page.title = page_data["title"]
+                page.save()
+
+            self.stdout.write(self.style.SUCCESS(
+                f'Page "{page.safe_translation_getter("title", any_language=True) or page.slug}" exists.'))
