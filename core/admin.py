b1269993234ab943f46bf7a17ac6854eaba71e1d
pavlo <frontbastard@gmail.com>
2025-02-21 14:43:57 +0100

    Add `Partners` functionality

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import SiteOptions
+from .models import SiteOptions, Partners
 
 
 @admin.register(SiteOptions)
 class SiteOptionsAdmin(admin.ModelAdmin):
-    list_display = ("sponsor_name", "sponsor_url", "sponsor_logo")
+    list_display = ["sponsor_name", "sponsor_url", "sponsor_logo"]
+
+
+@admin.register(Partners)
+class PartnersAdmin(admin.ModelAdmin):
+    list_display = ["name", "url"]
