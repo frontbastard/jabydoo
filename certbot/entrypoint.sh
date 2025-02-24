6b39c7b208527afa80c33d1c068920056a1c599c
pavlo <frontbastard@gmail.com>
2025-02-24 12:49:00 +0100

    Fix nginx config generation from template

--- certbot/entrypoint.sh
+++ certbot/entrypoint.sh
@@ -6,7 +6,7 @@ CERT_DIR="/etc/letsencrypt/live/$SITE_DOMAIN"
 # If certificates are missing, get new ones
 if [ ! -d "$CERT_DIR" ]; then
   echo "The certificates are missing. Run certbot..."
-  certbot certonly --webroot -w /var/www/certbot -d $SITE_DOMAIN -d www.$SITE_DOMAIN \
+  certbot certonly --webroot -w /var/www/$SITE_DOMAIN/certbot -d $SITE_DOMAIN -d www.$SITE_DOMAIN \
     --email your@email.com --agree-tos --no-eff-email --force-renewal
 
   echo "Restarting Nginx..."
