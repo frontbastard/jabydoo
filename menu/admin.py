bf69dee2a2e625d47656a1f449b3fd05fd154d35
pavlo <frontbastard@gmail.com>
2025-02-27 10:24:50 +0100

    Generate pages on creating menu items

--- menu/admin.py
+++ menu/admin.py
@@ -19,10 +19,14 @@ class MenuItemAdmin(TranslatableAdmin, DraggableMPTTAdmin):
         models.TextField: {"widget": Textarea(attrs={"style": "width: 100%;"})},
         models.CharField: {"widget": TextInput(attrs={"style": "width: 100%;"})},
     }
+    fields = ["name", "url", "parent", "order"]
     list_display = ["tree_actions", "indented_title", "url", "order"]
     list_display_links = ["indented_title"]
     list_editable = ["order"]
     mptt_level_indent = 20
+
+    def get_prepopulated_fields(self, request, obj=None):
+        return {"url": ("name",)}
 
 
 admin.site.register(MenuItem, MenuItemAdmin)
