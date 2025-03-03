003f2c31746035efde43d457644d8d1539e0cfbd
pavlo <frontbastard@gmail.com>
2025-03-03 16:48:41 +0100

    Separate different menus by filter

--- menu/admin.py
+++ menu/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.forms import TextInput
+from django.shortcuts import redirect
+from django.urls import reverse
 from mptt.admin import DraggableMPTTAdmin
 from mptt.forms import MPTTAdminForm
 from parler.admin import TranslatableAdmin
@@ -35,7 +37,17 @@ class MenuItemAdmin(TranslatableAdmin, DraggableMPTTAdmin):
     list_display = ["tree_actions", "indented_title", "menu", "url", "order"]
     list_display_links = ["indented_title"]
     list_editable = ["order"]
+    list_filter = ["menu"]
     mptt_level_indent = 20
 
     def get_prepopulated_fields(self, request, obj=None):
         return {"url": ("name",)}
+
+    def changelist_view(self, request, extra_context=None):
+        if "menu__id__exact" not in request.GET:
+            first_menu = Menu.objects.first()
+            if first_menu:
+                opts = self.model._meta
+                url = reverse(f"admin:{opts.app_label}_{opts.model_name}_changelist")
+                return redirect(f"{url}?menu__id__exact={first_menu.id}")
+        return super().changelist_view(request, extra_context)
